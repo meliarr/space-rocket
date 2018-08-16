52ade967ce607955e544e265737200b1bc82b9fa
MelissaA <37823114+meliarr@users.noreply.github.com>
2018-08-16 15:06:59 -0500

    adjusted JS timer

--- rocket.js
+++ rocket.js
@@ -1,20 +1,32 @@
+//timer
 var timer = null;
 var countdownNumber = 10;
 
 var changeState = function (state) {
     document.body.className = "body-state" + state;
-
     clearInterval(timer);
-
+    countdownNumber = 10;
+    document.getElementById('countdown').innerHTML = countdownNumber;
+    
+    // countdown
     if (state == 2) {
         timer = setInterval(function () {
-            document.getElementById('countdown').innerHTML = countdownNumber;
             countdownNumber = countdownNumber - 1;
+            document.getElementById('countdown').innerHTML = countdownNumber;
+            
             if (countdownNumber <= 0) {
                 changeState(3);
             }
         }, 1000);
     } else if (state == 3) {
-
+        var success = setTimeout(function ()
+            {
+                var randomNumber = Math.round(Math.random()*10);
+                if (randomNumber > 5) {
+                    changeState(4); //well done!
+                } else {
+                    changeState(5); // oh no!
+                }
+    }, 2000);
     };
 }
